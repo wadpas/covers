04b759f748a3722352711503db2813b0a3e6c273
wadpas <wadpas@gmail.com>
2024-05-14 15:44:43 +0300

    set routes & controllers folders

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const app = express()
-
+const tasks = require('./routes/tasks')
 const port = 3000
 
-app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`)
-})
+app.use(express.json())
 
 app.get('/', (req, res) => {
 	res.send('Hello World!')
+})
+
+app.use('/api/v1/tasks', tasks)
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`)
 })
---
+++ controllers/tasks.js
@@ -0,0 +1,7 @@
+const getAllTasks = (req, res) => {
+	res.send('All tasks')
+}
+
+module.exports = {
+	getAllTasks
+}
