605679bef63645d09e854d03f0368f9a6c3b56f4
wadpas <wadpas@gmail.com>
2024-06-27 09:55:24 +0300

    added products controller & router

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
+require('dotenv').config()
+require('express-async-errors')
+
 const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
+const products = require('./routes/products')
 const connectDB = require('./db/connect')
 const notFound = require('./middleware/not-found')
-const errorHandler = require('./middleware/not-found')
-require('dotenv').config()
-
+const errorHandler = require('./middleware/error-handler')
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-
 app.use('/api/v1/tasks', tasks)
+app.use('/api/v1/products', products)
 app.use(notFound)
 app.use(errorHandler)
 
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,11 +1,13 @@
 const { CustomApiError } = require('../errors/custom-error')
 
 const errorHandler = (err, req, res, next) => {
-	if (err instanceof CustomApiError) {
-		return res.status(err.statusCode).json({
-			message: err.message,
-		})
-	}
+	console.log(err)
+	// if (err instanceof CustomApiError) {
+	// 	return res.status(err.statusCode).json({
+	// 		message: err.message,
+	// 	})
+	// }
+
 	return res.status(500).json({
 		message: 'Something went wrong, please try again',
 	})
