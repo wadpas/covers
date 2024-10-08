540ab8f15364138a567bd4835e85650077462893
wadpas <wadpas@gmail.com>
2024-10-08 21:20:07 +0300

    done add Category form

---
+++ client/src/views/AddCategory.vue
@@ -0,0 +1,61 @@
+<template>
+	<h3 class="p-3">Add Category</h3>
+	<div class="p-3 w-50 text-primary-emphasis border border-light rounded-3">
+		<form>
+			<div class="mb-3">
+				<label
+					for="name"
+					class="form-label">
+					Name
+				</label>
+				<input
+					v-model="formData.name"
+					type="text"
+					class="form-control"
+					id="name" />
+			</div>
+			<div class="mb-3">
+				<label
+					for="description"
+					class="form-label">
+					Description
+				</label>
+				<textarea
+					v-model="formData.description"
+					type="text"
+					class="form-control"
+					id="description" />
+			</div>
+			<div class="mb-3">
+				<label
+					for="image"
+					class="form-label">
+					Image
+				</label>
+				<input
+					v-model="formData.imageUrl"
+					type="text"
+					class="form-control"
+					id="image" />
+			</div>
+			<button
+				type="submit"
+				class="btn btn-primary">
+				Submit
+			</button>
+		</form>
+	</div>
+</template>
+
+<script setup>
+	import { ref } from 'vue'
+	import axios from 'axios'
+
+	const formData = {
+		name: '',
+		description: '',
+		imageUrl: '',
+	}
+</script>
+
+<style lang="scss" scoped></style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -15,9 +15,7 @@
 				aria-describedby="emailHelp" />
 			<div
 				id="emailHelp"
-				class="form-text">
-				We'll never share your email with anyone else.
-			</div>
+				class="form-text"></div>
 		</div>
 		<div class="mb-3">
 			<label
