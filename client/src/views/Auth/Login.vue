df6e717c81b4ba04947cefefa5bdaf800787d439
wadpas <wadpas@gmail.com>
2024-10-13 14:02:02 +0300

    refresh login register page

--- client/src/views/Auth/Login.vue
+++ client/src/views/Auth/Login.vue
@@ -1,79 +1,57 @@
 <template>
-	<div class="flex flex-col justify-center flex-1 min-h-full px-6 py-12 lg:px-8">
-		<div class="sm:mx-auto sm:w-full sm:max-w-sm">
-			<h2 class="mt-10 text-2xl font-bold leading-9 tracking-tight text-center text-gray-900">
-				Sign in to your account
-			</h2>
-		</div>
-
-		<div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-			<form
-				class="space-y-6"
-				action="#"
-				method="POST">
-				<div>
-					<label
-						for="email"
-						class="block text-sm font-medium leading-6 text-gray-900">
-						Email address
-					</label>
-					<div class="mt-2">
-						<input
-							v-model="formData.email"
-							id="email"
-							name="email"
-							type="email"
-							required="true"
-							class="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-950 sm:text-sm sm:leading-6" />
-					</div>
-				</div>
-
-				<div>
-					<div class="flex items-center justify-between">
-						<label
-							for="password"
-							class="block text-sm font-medium leading-6 text-gray-900">
-							Password
-						</label>
-						<div class="text-sm">
-							<a
-								href="#"
-								class="font-semibold text-sky-950 hover:text-sky-900">
-								Forgot password?
-							</a>
-						</div>
-					</div>
-					<div class="mt-2">
-						<input
-							v-model="formData.password"
-							id="password"
-							name="password"
-							type="password"
-							required="true"
-							class="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-950 sm:text-sm sm:leading-6" />
-					</div>
-				</div>
-
-				<div>
-					<button
-						type="button"
-						@click="onSubmit"
-						class="flex w-full justify-center rounded-md bg-sky-950 px-3 p-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-sky-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-950">
-						Login
-					</button>
-				</div>
-			</form>
-
-			<p class="mt-10 text-sm text-center text-gray-500">
+	<div class="row justify-content-md-center">
+		<form class="m-5 col-4">
+			<h2 class="my-4 text-center">Sign in to account</h2>
+			<div class="mb-3">
+				<label
+					for="email"
+					class="form-label">
+					Email address
+				</label>
+				<input
+					v-model="formData.email"
+					type="email"
+					class="form-control"
+					id="email" />
+			</div>
+			<div class="mb-3">
+				<label
+					for="password"
+					class="form-label">
+					Password
+				</label>
+				<input
+					v-model="formData.password"
+					type="password"
+					class="form-control"
+					id="password" />
+			</div>
+			<div class="mb-3 form-check">
+				<input
+					type="checkbox"
+					class="form-check-input"
+					id="checkbox" />
+				<label
+					class="form-check-label"
+					for="checkbox">
+					Check me out
+				</label>
+			</div>
+			<button
+				type="button"
+				class="btn btn-dark"
+				@click="onSubmit">
+				Submit
+			</button>
+			<p class="my-3 text-center">
 				Not a member?
-				{{ ' ' }}
 				<router-link
 					:to="{ name: 'auth-register' }"
-					class="font-semibold leading-6 text-sky-950 hover:text-sky-900">
+					class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
 					Start free
 				</router-link>
 			</p>
-		</div>
+		</form>
 	</div>
 </template>
 
@@ -92,7 +70,7 @@
 	async function onSubmit() {
 		try {
 			await store.loginUser(formData)
-			router.push('/')
+			router.push({ name: 'home' })
 		} catch (error) {
 			console.log(error)
 		}
